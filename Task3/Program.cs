5ed68324580d29e69813d654e1c9a67ae3d55da3
Ageev Ilya <Aii23@mail.ru>
2023-10-14 12:12:48 +0300

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите целое число ");
+string number_str = Console.ReadLine();
+int number = Convert.ToInt32(number_str);
+for(int N = -number; N <= number; N++)
+{
+    Console.Write(N + " ");
+}
+
+// int number = Convert.ToInt32(Console.ReadLine());
+// int index = 0;
+// if (number > 0)
+// {
+//     index = -number;
+// }
+// else 
+// {
+//     index = number;
+//     number = - number;
+// }
+// while (index < number + 1 )
+// {
+//     Console.Write(index);
+//     Console.Write(" ");
+//     index = index + 1;
+// }
