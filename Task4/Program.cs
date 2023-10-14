76a9ae744a3e4c1ec70d14bf42475b0ef1162003
Ageev Ilya <Aii23@mail.ru>
2023-10-14 12:12:57 +0300

    Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number >= 100 && number < 1000)
+{
+    int lastnumber = number % 10;
+    Console.WriteLine(lastnumber);
+}
+else
+{
+    Console.WriteLine("Введите число меньше 1000, но больше 100");
+}
