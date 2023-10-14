12475cb7546e39058fe9d9d6c75672b140542e9b
Ageev Ilya <Aii23@mail.ru>
2023-10-14 12:12:25 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите первое число ");
+string number1_str = Console.ReadLine();
+int number1 = Convert.ToInt32(number1_str);
+Console.WriteLine("Введите второе число ");
+string number2_str = Console.ReadLine();
+int number2 = Convert.ToInt32(number2_str);
+int kvadrat = number2 * number2;
+if(kvadrat == number1)
+{
+    Console.Write("Да");
+}
+else
+{
+    Console.Write("Нет");
+}  
