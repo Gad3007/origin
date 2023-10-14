36584940ddc93a4f520194b3ab430124029cd65b
Ageev Ilya <Aii23@mail.ru>
2023-10-14 12:12:40 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("Введите номер от 1 до 7 ");
+string num1 = Console.ReadLine();
+if(num1 == "1")
+{
+    Console.WriteLine("Понедельник");
+}
+
+else if(num1 == "2")
+{
+    Console.WriteLine("Вторник");
+}
+else if(num1 == "3")
+{
+    Console.WriteLine("Среда");
+}
+else if(num1 == "4")
+{
+    Console.WriteLine("Четверг");
+}
+else if(num1 == "5")
+{
+    Console.WriteLine("Пятница");
+}
+else if(num1 == "6")
+{
+    Console.WriteLine("Суббота");
+}
+else if(num1 == "7")
+{
+    Console.WriteLine("Воскресенье");
+}
