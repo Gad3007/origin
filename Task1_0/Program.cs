d24d70e43888808ea536465462572f2c0b689b02
Ageev Ilya <Aii23@mail.ru>
2023-10-15 11:31:56 +0300

    Task1_0

---
+++ Task1_0/Program.cs
@@ -0,0 +1,12 @@
+﻿Random generatenumber = new Random();
+int number = generatenumber.Next(100, 1000);
+Console.WriteLine(number);
+int numberA = number / 100 ;
+Console.WriteLine(numberA);
+int numberB = number % 10;
+Console.WriteLine(numberB);
+// Console.WriteLine(numberA.ToString() + numberB.ToString());
+Console.WriteLine(numberA + numberB);
+Console.WriteLine(numberA + "" + numberB);
+Console.WriteLine($"{numberA}" + $"{numberB}");
+Console.WriteLine(numberA * 10 + numberB);
