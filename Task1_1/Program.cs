5c11aa6b945a593066d9c57eb6f1fe3d319f25c3
Ageev Ilya <Aii23@mail.ru>
2023-10-15 11:32:08 +0300

    Task1_1

---
+++ Task1_1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Number1 - ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Number2 - ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int result = num1 % num2 ;
+if(result == 0)
+{
+    Console.Write("Первое число кратно второму ");
+}
+else
+{
+    Console.Write("Первое число некратно второму, остаток " + result);
+}
