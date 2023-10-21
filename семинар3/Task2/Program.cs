edb48a97de7d466a966ac6375ec386ac483f1fab
Ageev Ilya <Aii23@mail.ru>
2023-10-21 12:32:22 +0300

    Task2

---
+++ семинар3/Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = new int[10];
+int len_arr = array.Length;
+
+for(int i = 0; i < len_arr; i++)
+{
+    array[i] = new Random().Next(-10, 11);
+    Console.Write(array[i] + " ");
+}
+
+
+Console.WriteLine("Преобразованный массив:  ");
+for(int i = 0; i < len_arr; i++)
+{
+    array[i] = -array[i];
+    Console.Write(array[i] + " ");
+}
+
