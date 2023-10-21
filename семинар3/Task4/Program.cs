744f41565fa721d6bda0c31f8b92f52099680e99
Ageev Ilya <Aii23@mail.ru>
2023-10-21 12:31:43 +0300

    Task4

---
+++ семинар3/Task4/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите трехзначное число - ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[3];
+
+
+
+if (num >= 100 && num < 1000)
+{
+    for(int i = 0; i < 3; i++)
+    {
+        array[i] = num % 10;
+        num /= 10;
+
+        Console.Write(array[i] + " ");
+    }
+
+    
+}
+else Console.WriteLine("Вы ввели не трехзначное число");
