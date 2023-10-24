a58c2ec0686398d0124b6727f9338f7498410b35
Ageev Ilya <Aii23@mail.ru>
2023-10-24 21:10:19 +0300

    Массивы и методы примеры

---
+++ МассивыПримеры/Пример3/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = {1,2,3,4,27,27,27,18};
+
+int n = array.Length;
+int find = 27;
+int index = 0;
+
+while(index < n)
+{   
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
---
+++ МассивыПримеры/Пример4/Program.cs
@@ -0,0 +1,50 @@
+﻿
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while(index < length)
+    {
+        collection[index] = new Random().Next(1,10);
+        index++;
+    }
+}
+
+void PrintArray (int[] arr) 
+{
+    int count = arr.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.Write($"{arr[position]} ");
+        position++;
+    }
+
+}
+
+int IndexOf(int[] collect, int find)
+{
+    int count = collect.Length;
+    int index = 0;
+    int pos = -1;
+
+    while (index < count)
+    {
+        if(collect[index] == find)
+        {
+            pos = index;
+            break;
+        }
+        index++;
+    }
+    return pos;
+}
+
+int[] mass = new int[10];//new int[10] создай новый массив в котором 10 элементов
+FillArray(mass);
+PrintArray(mass);
+Console.WriteLine();
+
+int pos = IndexOf(mass, 4);
+Console.WriteLine(pos);
