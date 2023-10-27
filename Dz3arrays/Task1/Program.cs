6a50baf9532d52e5f8922fb80478125d9104551e
Ageev Ilya <Aii23@mail.ru>
2023-10-27 13:35:11 +0300

    массивы дз

---
+++ Dz3arrays/Task1/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 1: Задайте одномерный массив из 10 целых чисел от 1 до 100. Найдите количество элементов массива, значения которых лежат в отрезке [20,90].
+// массив [10 21 14 93 23] => 2
+
+// 1.Задать одномерный массив из 10 целых чисел от 1 до 100 
+// 2.Задать диапозон поиска элементов
+// 3.Выдать кол-во найденных элементов
+
+
+int[] GenerateArray()
+{
+    int[] array = new int[10];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 100);
+    }
+
+    return array;
+}
+void PrintArray(int[] array)
+{
+    Console.Write($"Сгенерированный массив размерностью {array.Length} элементов : ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+int KolvoElem(int[] array)
+{
+    int Kolvo = 0;
+    int index = 0;
+    while(index < array.Length)
+    {
+        if(array[index] >= 20 && array[index] <= 90)
+        {
+            Kolvo++;
+        }
+        index++;
+    }
+    return Kolvo;
+}
+int[] mass = GenerateArray();
+PrintArray(mass);
+// int N = KolvoElem(mass);
+Console.Write($"КОЛИЧЕСТВО ЭЛЕМЕНТОВ = {KolvoElem(mass)}");
---
+++ Dz3arrays/Task3/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 3: Задайте массив из вещественных чисел с ненулевой дробной частью. Найдите разницу между максимальным и минимальным элементов массива.
+// массив [2.2 0.4 9.11 7.2 78.98] => 78.58
+// массив [1.22 4.5 3.33] => 3.28
+
+double[] GenerateArray()
+{
+    double[] array = new double[5];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Random rnd = new Random();
+        array[i] = Math.Round((0.01 + rnd.NextDouble() * (0.01 + 100.00)),2);
+        
+    }
+
+    return array;
+}
+
+void PrintArray(double[] array)
+{
+    Console.Write($"Сгенерированный массив размерностью {array.Length} элементов : ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+}
+
+void MaxMin(double[] array)
+{
+    double max = 0;
+    double min = array[0];
+    for(int i = 0; i < array.Length;i++)
+    {
+        if(array[i] > max)
+        {
+            max = array[i];
+        }
+        
+    }
+    for(int i = 0; i < array.Length;i++)
+    {
+        if(array[i] < min)
+        {
+            min = array[i];
+        }
+        
+    }
+   
+    double result = max - min; 
+    Console.Write($"МАКСИМАЛЬНЫЙ {max} - МИНИМАЛЬНЫЙ {min} = РЕЗУЛЬТАТ {result}");
+}
+
+double[] mass = GenerateArray();
+PrintArray(mass);
+MaxMin(mass);
+
+
