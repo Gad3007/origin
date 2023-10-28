13601b80bf52bc2e45e5f378f882efab07fb5307
Ageev Ilya <Aii23@mail.ru>
2023-10-28 12:18:33 +0300

    пример 2

---
+++ ДвумерныеМассивы/Пример2/Program.cs
@@ -0,0 +1,42 @@
+﻿//Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
+
+
+int [,] array = new int[6,6];
+
+for (int i = 0; i < array.GetLength(0); i++)//8
+    {
+        for (int j = 0; j < array.GetLength(1); j++)//6
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+
+for (int i = 0; i < array.GetLength(0); i++)//8
+    {
+        for (int j = 0; j < array.GetLength(1); j++)//6
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+
+int result = 0;
+// for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             if( i == j)
+//             {
+//                 result = result  + array[i,j];
+
+//             }
+             
+//         }    
+//     }    
+for (int i = 0, j = 0; i < array.GetLength(0) && j < array.GetLength(1); j++, i++)
+{
+    result = result  + array[i,j];
+} 
+
+Console.Write($"Cумма элементов: {result}");
+
